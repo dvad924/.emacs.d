ba1933dc68b80f918aa81208daa00e5d311b9870
David Vadney <dvad924@gmail.com>
2018-10-24 21:56:09 -0400

    Vastly simplifying config

--- elpa/gulp-task-runner-20161103.1523/gulp-task-runner-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gulp-task-runner" "20161103.1523" "Gulp task runner" 'nil :commit "f13da9e619c1838571df0a0462c273ed6e76defc" :keywords '("convenience" "javascript"))
--- elpa/widgetjs-20160719.804/widgetjs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "widgetjs" "20160719.804" "Widgetjs mode" '((makey "0.3") (js2-mode "20140114") (js2-refactor "0.6.1") (s "1.9.0")) :commit "74daf284e32bf84d29377d1d7a638b640ebf8f76" :keywords '("help"))
